e184a0173d2c2e020cdce43f0a30cae1f1a8726d
ViktorGuschin <groftblr@gmail.com>
2019-03-02 17:20:44 +0300

    98 / 100

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  let index2 = 2;
-  let index5 = 5;
-  let res2 = 0;
-  let res5 = 0;
+  let index = 0;
   let power = [];
+  let res = 0;
 
   for (let p = 2; p <= base; ++p) {
     if (base % p === 0) {
       power.push(p);
       base = Math.floor(base / p);
-      p = 2;
+      p = 1;
     }
   }
-  // while (index <= base) {
-  // power = Math.floor(base / index5);
-  //   index *= 5;
-  // }
-  console.log(power);
-  // index = power;
-  while (index2 <= number) {
-    res2 += Math.floor(number / index2);
-    index2 *= 2;
+  const maxPower = Math.max.apply(null, power);
+  const maxPowerQuantity = power.reduce((acc, el) => {
+    if (el === maxPower) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+  index = maxPower;
+  while (index <= number) {
+    res += Math.floor(number / index);
+    index *= maxPower;
   }
-  while (index5 <= number) {
-    res5 += Math.floor(number / index5);
-    index5 *= 5;
-  }
-  // console.log(arr);
-  console.log(res2, res5, res2 < res5 ? res2 : res5);
+  // console.log(maxPower);
+  // console.log(res);
+  return Math.floor(res / maxPowerQuantity);
 };
