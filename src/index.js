ae53ce3a6a999a281c6ecf83916c43ad4e65c0ce
ViktorGuschin <groftblr@gmail.com>
2019-03-02 16:06:39 +0300

    decomposition base into prime factors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  let index2 = 2;
+  let index5 = 5;
+  let res2 = 0;
+  let res5 = 0;
+  let power = [];
+
+  for (let p = 2; p <= base; ++p) {
+    if (base % p === 0) {
+      power.push(p);
+      base = Math.floor(base / p);
+      p = 2;
+    }
+  }
+  // while (index <= base) {
+  // power = Math.floor(base / index5);
+  //   index *= 5;
+  // }
+  console.log(power);
+  // index = power;
+  while (index2 <= number) {
+    res2 += Math.floor(number / index2);
+    index2 *= 2;
+  }
+  while (index5 <= number) {
+    res5 += Math.floor(number / index5);
+    index5 *= 5;
+  }
+  // console.log(arr);
+  console.log(res2, res5, res2 < res5 ? res2 : res5);
+};
